7ab66e52c4e07f904ef54c95cf93010870bcae70
Robin Mattheussen <robin.mattheussen@gmail.com>
2018-08-02 19:11:01 +0200

    Now includes very basic XMonad config

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,5 +1,35 @@
 import XMonad
 
-main :: IO()
-main =
-  xmonad def {terminal = "gnome-terminal", modMask = mod4Mask, borderWidth = 5}
+import XMonad.Actions.Navigation2D
+
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.DynamicLog
+
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.Spacing
+import XMonad.Layout.ThreeColumns
+
+main :: IO ()
+main = xmonad =<< xmobar myConfig
+
+myXmobarCommand :: String
+myXmobarCommand = "xmobar"
+
+myXmobarPP = xmobarPP { ppCurrent = xmobarColor backgroundColor "" . wrap "<" ">" }
+
+-- TODO: Get these colors from xrdb
+backgroundColor   = "#FEFEFE"
+middleColor       = "#AEAEAE"
+foregroundColor   = "#0E0E0E"
+
+myConfig = ewmh def
+  { borderWidth        = 1
+  , focusedBorderColor = foregroundColor
+  , focusFollowsMouse  = False
+  , handleEventHook    = fullscreenEventHook
+  , layoutHook         = spacingWithEdge 10 emptyBSP ||| spacingWithEdge 10 (ThreeColMid 1 (3/100) (2/3))
+  , modMask            = mod4Mask
+  , normalBorderColor  = middleColor
+  , terminal           = "tilix"
+  , workspaces         = [ "browse", "code", "read", "chat", "etc" ]
+  }
