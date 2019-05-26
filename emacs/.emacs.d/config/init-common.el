3bb493d495e0864f4a5c25140877b03ca7511cce
Robin Mattheussen <robin.mattheussen@gmail.com>
2019-05-27 01:38:57 +0200

    Enabled Smartparens mode in Emacs

--- emacs/.emacs.d/config/init-common.el
+++ emacs/.emacs.d/config/init-common.el
@@ -15,7 +15,15 @@
 ;; Immediately display keystrokes in the minibuffer
 (setq echo-keystrokes 0.1)
 
-;; Shows a popup for possible key combinations
+;; which-key, shows a popup for possible key combinations
 (use-package which-key
   :straight t
   :init (which-key-mode))
+
+;; smartparens, smart editing with parenthesis
+(use-package smartparens
+  :straight t
+  :config
+  ;; Smartparens doesn't use autoloads so we still need package.el
+  (use-package smartparens-config :straight nil)
+  (smartparens-global-mode))
