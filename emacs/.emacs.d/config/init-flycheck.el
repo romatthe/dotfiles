28912eaf0eea48210c7abdc7b917f08bed86a3d5
Robin Mattheussen <robin.mattheussen@gmail.com>
2019-05-28 01:30:42 +0200

    Emacs: Added and configured Flycheck

---
+++ emacs/.emacs.d/config/init-flycheck.el
@@ -0,0 +1,46 @@
+;;; -*- lexical-binding: t -*-
+
+;; flycheck, an error checking framework
+(use-package flycheck
+  :straight t
+  :init
+  ;; Draw a custom bitmap for the indicators, more in line with Jetbrains IDEs
+  (define-fringe-bitmap 'flycheck-fringe-indicator
+    (vector #b0000000000000000
+	    #b0000000000000000
+	    #b0000000000000000
+	    #b0000000000000000
+	    #b0000000000000000
+	    #b1111111111111111
+	    #b1111111111111111
+	    #b1111111111111111
+	    #b1111111111111111
+	    #b1111111111111111
+	    #b1111111111111111
+	    #b0000000000000000
+	    #b0000000000000000
+	    #b0000000000000000
+	    #b0000000000000000
+	    #b0000000000000000
+	    #b0000000000000000)
+    nil 16)
+  ;; Draw the indicators on the right side, also in line with Jetbrains IDEs
+  :custom (flycheck-indication-mode 'right-fringe)
+  ;; Using global mode is the recommended way to use flycheck
+  hook ((after-init . global-flycheck-mode))
+  :config
+  (flycheck-define-error-level 'error
+			       :severity 2
+			       :overlay-category 'flycheck-error-overlay
+			       :fringe-bitmap 'flycheck-fringe-indicator
+			       :fringe-face 'flycheck-fringe-error)
+  (flycheck-define-error-level 'warning
+			       :severity 1
+			       :overlay-category 'flycheck-warning-overlay
+			       :fringe-bitmap 'flycheck-fringe-indicator
+			       :fringe-face 'flycheck-fringe-warning)
+  (flycheck-define-error-level 'info
+			       :severity 0
+			       :overlay-category 'flycheck-info-overlay
+			       :fringe-bitmap 'flycheck-fringe-indicator
+			       :fringe-face 'flycheck-fringe-info))
