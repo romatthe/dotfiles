49cc8d447e094c8235505a5b8497eda882b5590b
Robin Mattheussen <robin.mattheussen@gmail.com>
2019-05-28 02:03:21 +0200

    Emacs: More sane defaults for history, autosave and backups

--- emacs/.emacs.d/config/init-common.el
+++ emacs/.emacs.d/config/init-common.el
@@ -27,3 +27,21 @@
   ;; Smartparens doesn't use autoloads so we still need package.el
   (use-package smartparens-config :straight nil)
   (smartparens-global-mode))
+
+;; Sane defaults for backups and autosaves
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups"))
+      delete-old-versions -1
+      version-control t
+      vc-make-backup-files t
+      auto-save-list-file-prefix "~/.emacs.d/autosave/"
+      auto-save-file-name-transforms '((".*" "~/.emacs.d/autosave/" t)))
+
+;; Sane defaults for history
+(setq savehist-file "~/.emacs.d/savehist"
+      history-length t
+      history-delete-duplicates t
+      savehist-save-minibuffer-history 1
+      savehist-additional-variables '(kill-ring search-ring regexp-search-ring)
+      recentf-max-saved-items 50)
+(savehist-mode 1)
+(recentf-mode 1)
